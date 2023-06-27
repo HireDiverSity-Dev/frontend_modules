fc20eb4140b9de63610e8266c90dfd340b5c2e0b
ryou <ryou@ryouui-MacBookAir.local>
2023-06-27 13:57:18 +0900

    FIX: ParagraphForm UI 타입에 맞게 수정

--- components/FormUI/ParagraphForm/index.tsx
+++ components/FormUI/ParagraphForm/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FormUIProps } from '@/models/FormUI/FormUI';
-import { ParagraphFormObj } from '@/models/FormUI/FormUIData';
+import { Paragraph_FormUIData } from '@/models/FormUI/FormUIData';
 
 function ParagraphForm({ form, uiSetting, lang }: FormUIProps) {
-  const formData = uiSetting.formData as ParagraphFormObj;
-  return <div className="paragraph" dangerouslySetInnerHTML={{ __html: formData.text[lang] }} />;
+  const formData = uiSetting.data as Paragraph_FormUIData;
+  return <div className="paragraph" dangerouslySetInnerHTML={{ __html: formData.text[lang] ?? '' }} />;
 }
 export default ParagraphForm;
