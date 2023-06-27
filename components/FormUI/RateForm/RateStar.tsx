ab9a8726049e683ec75410a2287e4e73fb6bf222
ryou <ryou@ryouui-MacBookAir.local>
2023-06-27 13:56:57 +0900

    FIX: RateForm UI 타입에 맞게 수정, 컴포넌트 분리

---
+++ components/FormUI/RateForm/RateStar.tsx
@@ -0,0 +1,34 @@
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import StarIcon from '@mui/icons-material/Star';
+
+interface RateStarProps {
+  curValue: number;
+  value: number;
+  onClick: (num: number) => void;
+}
+
+export default function RateStar({ curValue, value, onClick }: RateStarProps) {
+  if (curValue < value) {
+    // unselected
+    return (
+      <StarOutlineIcon
+        fontSize="inherit"
+        sx={{ color: '#FFD700' }}
+        onClick={() => {
+          onClick(value);
+        }}
+      />
+    );
+  } else {
+    // selected
+    return (
+      <StarIcon
+        fontSize="inherit"
+        sx={{ color: '#FFD700' }}
+        onClick={() => {
+          onClick(value);
+        }}
+      />
+    );
+  }
+}
--- components/FormUI/RateForm/index.tsx
+++ components/FormUI/RateForm/index.tsx
@@ -1,52 +1,35 @@
 import React, { useEffect } from 'react';
-import StarIcon from '@mui/icons-material/Star';
-import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import { Box } from '@mui/material';
 import { useController } from 'react-hook-form';
-import { FormProps } from '..';
+import { FormUIProps } from '@/models/FormUI/FormUI';
+import RateStar from '@/components/FormUI/RateForm/RateStar';
 
-function RateForm({ pageForm, setting }: FormProps) {
+function RateForm({ form, uiSetting }: FormUIProps) {
   const { field } = useController({
-    name: setting.formKey,
-    control: pageForm.control,
-    rules: setting.rules,
+    name: uiSetting.formKey,
+    control: form.control,
+    rules: {
+      required: uiSetting.rule?.required,
+    },
   });
 
   const onClickHandler = (num: number) => {
-    if (!setting.rules?.default) {
+    if (!uiSetting.rule?.default) {
       field.onChange(num);
     }
   };
 
   useEffect(() => {
-    if (setting.rules?.default && setting.defaultValue) {
-      field.onChange(setting.defaultValue);
+    if (uiSetting.rule?.default && uiSetting.defaultValue) {
+      field.onChange(uiSetting.defaultValue);
     }
-  }, [setting.rules?.default, setting.defaultValue]);
+  }, [uiSetting.rule?.default, uiSetting.defaultValue]);
 
   return (
     <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', fontSize: '50px' }}>
-      {[1, 2, 3, 4, 5].map((val, index) =>
-        !field?.value || field.value < val ? (
-          <StarOutlineIcon
-            fontSize="inherit"
-            sx={{ color: '#FFD700' }}
-            key={index}
-            onClick={() => {
-              onClickHandler(val);
-            }}
-          />
-        ) : (
-          <StarIcon
-            fontSize="inherit"
-            sx={{ color: '#FFD700' }}
-            key={index}
-            onClick={() => {
-              onClickHandler(val);
-            }}
-          />
-        ),
-      )}
+      {[1, 2, 3, 4, 5].map((value, index) => (
+        <RateStar curValue={field?.value ? (field.value as number) : 0} value={value} onClick={onClickHandler} />
+      ))}
     </Box>
   );
 }
