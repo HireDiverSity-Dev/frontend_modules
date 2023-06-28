5528f69c03a7861d299d5fae3a5e92f810d4fe7c
ryou <ryou@ryouui-MacBookAir.local>
2023-06-28 16:56:22 +0900

    FEAT: formatDate 함수 추가

--- utils/date.ts
+++ utils/date.ts
@@ -9,5 +9,16 @@ function getCurrentDate() {
 
   return currentDate;
 }
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
 
-export { getCurrentDate };
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hour}:${minute}`;
+};
+
+export { formatDate, getCurrentDate };
