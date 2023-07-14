5490d9460ca093156e836ba54054b50035f033b1
Developerryou <developerryou@gmail.com>
2023-07-14 16:34:40 +0900

    FIX: semiTitle link 안걸리는 이슈 수정

--- components/FormUI/_elements/FormSemiTitle.tsx
+++ components/FormUI/_elements/FormSemiTitle.tsx
@@ -9,6 +9,20 @@ interface FormSemiTitleProps {
 }
 
 function FormSemiTitle({ uiSetting, lang }: FormSemiTitleProps) {
+  const setLink = (subtitle: string) => {
+    let changed = subtitle;
+    const link_regex = /<[^|<>]+[|][^|<>]+>/g;
+    const links = Array.from(changed.matchAll(link_regex));
+    links.forEach(([fulltext]: Array<string>) => {
+      const text = fulltext.split('|')[0].slice(1);
+      let link = fulltext.split('|')[1];
+      link = fulltext.split('|')[1].slice(0, link.length - 1);
+
+      changed = changed.replace(fulltext, `<a href="${link}" target="_blank">${text}</a>`);
+    });
+    return changed;
+  };
+
   if (uiSetting.data?.subtitle === undefined) return <></>;
   return (
     <Typography
@@ -16,7 +30,7 @@ function FormSemiTitle({ uiSetting, lang }: FormSemiTitleProps) {
       mb={1}
       px={1}
       dangerouslySetInnerHTML={{
-        __html: uiSetting.data.subtitle?.[lang] ?? '',
+        __html: setLink(uiSetting.data.subtitle?.[lang] ?? ''),
       }}
     ></Typography>
   );
