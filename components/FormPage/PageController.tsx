aa1250679ef3766573ed8da38a9cd2939b6ad4ff
Developerryou <developerryou@gmail.com>
2023-07-14 17:29:12 +0900

    FIX: 페이지 movable 함수 추가해 이동가능 상태인지 판단

--- components/FormPage/PageController.tsx
+++ components/FormPage/PageController.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { Button } from '@mui/material';
+import isMovable from 'fe-modules/components/FormPage/isMovable';
 import SetSetting from 'fe-modules/components/FormUI/SetSetting';
 import { FormUISetting, FormUIUseFormReturn } from 'fe-modules/models/FormUI/FormUI';
 import { useTranslation } from 'next-i18next';
+import { useWatch } from 'react-hook-form';
 
 interface Props {
   form: FormUIUseFormReturn;
@@ -31,24 +33,21 @@ function nextPage({ page, setPage, endPage }: Props) {
 function PageController(props: Props) {
   const { t } = useTranslation('customForm');
   const { form, uiSettings, page, endPage } = props;
-  const [isFilled, setIsFilled] = useState(false);
   const newSettings = uiSettings.map((uiSetting) => {
     const newSetting = SetSetting(form, uiSetting);
     return newSetting;
   });
 
+  const watch = useWatch({ control: form.control });
+  const [canMoveNext, setCanMoveNext] = useState(false);
   useEffect(() => {
-    setIsFilled(true);
-    newSettings.map((newSetting) => {
-      if (newSetting.page === page) {
-        const value = form.watch(newSetting.formKey);
-        console.log(newSetting.formKey, value);
-        const { invalid } = form.getFieldState(newSetting.formKey);
-        if (!newSetting.rule?.invisible) {
-          if ((newSetting.rule?.required && value === undefined) || invalid) setIsFilled(false);
-        }
-      }
-    });
+    const tempMoveNext = isMovable(
+      form,
+      newSettings.filter((newSetting) => newSetting.page === page),
+      form.watch(`pages.${page}.conditions`),
+      watch,
+    );
+    setCanMoveNext(tempMoveNext);
   }, [page, uiSettings, form.watch()]);
 
   return (
@@ -57,7 +56,7 @@ function PageController(props: Props) {
         <ChevronLeftIcon />
         {t('폼.페이지이동.이전')}
       </Button>
-      <Button disabled={page === endPage || !isFilled} onClick={() => nextPage(props)}>
+      <Button disabled={page === endPage || !canMoveNext} onClick={() => nextPage(props)}>
         {t('폼.페이지이동.다음')}
         <ChevronRightIcon />
       </Button>
---
+++ components/FormPage/isMovable.ts
@@ -0,0 +1,33 @@
+import makeNewRule from 'fe-modules/components/FormUI/makeNewRule';
+import { FormUISetting, FormUIUseFormReturn } from 'fe-modules/models/FormUI/FormUI';
+import { FormUICondition } from 'fe-modules/models/FormUI/FormUICondition';
+import { FormUIRule } from 'fe-modules/models/FormUI/FormUIRule';
+
+// TODO: 매개변수 최적화
+
+function isMovable(
+  form: FormUIUseFormReturn,
+  settings: FormUISetting[],
+  pageConditions: Array<FormUICondition>,
+  watch: Record<string, any>,
+) {
+  let tempMoveNext = true;
+  settings.map((setting) => {
+    const value = form.watch(setting.formKey);
+    const { invalid } = form.getFieldState(setting.formKey);
+    if (!setting.rule?.invisible) {
+      if ((setting.rule?.required && value === undefined) || invalid) tempMoveNext = false;
+    }
+  });
+
+  if (tempMoveNext) {
+    let pageRule: FormUIRule = {
+      invisible: false,
+    };
+    pageRule = makeNewRule(pageConditions ?? [], pageRule, watch);
+    tempMoveNext = pageRule.invisible ? false : true;
+  }
+  return tempMoveNext;
+}
+
+export default isMovable;
