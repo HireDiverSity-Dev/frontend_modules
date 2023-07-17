3017f8fa31ca589c40535900e4ebcf31958f116e
Developerryou <developerryou@gmail.com>
2023-07-17 13:48:14 +0900

    FIX: 이미지 업로드 에러 발생시 throw

--- apis/s3/file.ts
+++ apis/s3/file.ts
@@ -1,49 +1,39 @@
 import { aws } from 'fe-modules/apis/network';
 
-export const uploadFileToPublicS3 = async (path: string, file: File, callbackFunc?: (() => void) | undefined) => {
-  try {
-    const res = await aws.post('/s3/public/signedUrl', {
-      key: path,
-      type: file.type,
-    });
-    const signedUrl = res.data.signedUrl;
-    console.log(signedUrl);
+export const uploadFileToPublicS3 = async (path: string, file: File) => {
+  const res = await aws.post('/s3/public/signedUrl', {
+    key: path,
+    type: file.type,
+  });
+  const signedUrl = res.data.signedUrl;
+  console.log(signedUrl);
 
-    await fetch(signedUrl, {
-      method: 'PUT',
-      body: file,
-      headers: {
-        'Content-type': file.type,
-      },
-    });
-    if (callbackFunc) {
-      callbackFunc();
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  const uploadRes = await fetch(signedUrl, {
+    method: 'PUT',
+    body: file,
+    headers: {
+      'Content-type': file.type,
+    },
+  });
+  if (uploadRes.status !== 200) throw new Error('upload failed');
+  return uploadRes;
 };
 
-export const uploadFileToPrivateS3 = async (path: string, file: File, callbackFunc?: (() => void) | undefined) => {
-  try {
-    const res = await aws.post('/s3/private/signedUrl', {
-      key: path,
-      type: file.type,
-    });
-    const signedUrl = res.data.signedUrl;
-    console.log(signedUrl);
+export const uploadFileToPrivateS3 = async (path: string, file: File) => {
+  const res = await aws.post('/s3/private/signedUrl', {
+    key: path,
+    type: file.type,
+  });
+  const signedUrl = res.data.signedUrl;
+  console.log(signedUrl);
 
-    await fetch(signedUrl, {
-      method: 'PUT',
-      body: file,
-      headers: {
-        'Content-type': file.type,
-      },
-    });
-    if (callbackFunc) {
-      callbackFunc();
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  const uploadRes = await fetch(signedUrl, {
+    method: 'PUT',
+    body: file,
+    headers: {
+      'Content-type': file.type,
+    },
+  });
+  if (uploadRes.status !== 200) throw new Error('upload failed');
+  return uploadRes;
 };
