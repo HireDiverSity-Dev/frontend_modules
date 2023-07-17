a27388ebc8eca7903483b92961fd63c7175c6549
Developerryou <developerryou@gmail.com>
2023-07-17 14:14:46 +0900

    REFACT: 업로드 기능 함수 분리

---
+++ components/FormUI/FileForm/fileUploadRequest.ts
@@ -0,0 +1,28 @@
+import { uploadFileToPrivateS3 } from 'fe-modules/apis/s3/file';
+import { getFileType } from 'fe-modules/components/FormUI/_checkFormUI/getFileType';
+import { ImageObj } from 'fe-modules/components/FormUI/FileForm/index';
+import { getCurrentDate } from 'fe-modules/utils/date';
+import { getRandomString } from 'fe-modules/utils/function';
+
+async function fileUploadRequest(file: File, filePath: string, fileNumber: number) {
+  const fileType = getFileType(file);
+  if (fileType === 'other') throw new Error('지원하지 않는 파일 형식입니다.');
+  const objectUrl = URL.createObjectURL(file);
+  const curDate = getCurrentDate();
+  const tag = getRandomString();
+  const path = `temp/${filePath}/${curDate}_file${tag}${String(fileNumber).padStart(2, '0')}`;
+
+  const imageObj: ImageObj = {
+    localPath: objectUrl,
+    s3Path: path,
+    name: file.name,
+    type: fileType,
+    size: file.size,
+    loading: false,
+  };
+
+  await uploadFileToPrivateS3(imageObj.s3Path, file);
+  return imageObj;
+}
+
+export default fileUploadRequest;
