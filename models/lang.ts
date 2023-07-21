dc2d83377866c5a810f0029371e3b3047c434e31
Developerryou <developerryou@gmail.com>
2023-07-21 14:48:34 +0900

    FEAT: 언어 변환함수 추가

--- models/lang.ts
+++ models/lang.ts
@@ -16,6 +16,23 @@ export const enum LangServer {
   VNM = '베트남어',
 }
 
+export const langConvertServer = (lang: Lang) => {
+  switch (lang) {
+    case Lang.KOR:
+      return LangServer.KOR;
+    case Lang.ENG:
+      return LangServer.ENG;
+    case Lang.CHN:
+      return LangServer.CHN;
+    case Lang.JPN:
+      return LangServer.JPN;
+    case Lang.VNM:
+      return LangServer.VNM;
+    default:
+      return LangServer.ENG;
+  }
+};
+
 export const langConvert = (lang: LangServer) => {
   switch (lang) {
     case LangServer.KOR:
