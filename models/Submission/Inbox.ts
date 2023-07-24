f2bb397e4ced5b1543054b97f6f06a062ef63e18
Developerryou <developerryou@gmail.com>
2023-07-24 16:09:21 +0900

    FIX: inbox 이름을 submission으로 변경

--- models/Submission/Inbox.ts
+++ models/Submission/Inbox.ts
@@ -1,6 +1,6 @@
 import { FormUIValue } from 'fe-modules/models/FormUI/FormUIValue';
 
-export interface Inbox {
+export interface Submission {
   _id: string;
   FormPage_id: string;
   data: { FormItem_id: string; value: FormUIValue }[];
