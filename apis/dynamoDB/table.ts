8bf12aa772320ccbecde66be7ce69d7ad5d99977
Developerryou <developerryou@gmail.com>
2023-07-24 14:43:41 +0900

    FIX: 정렬 키 추가 할 수 있도록 수정

--- apis/dynamoDB/table.ts
+++ apis/dynamoDB/table.ts
@@ -1,11 +1,11 @@
 import { AxiosResponse } from 'axios';
 import { aws } from 'fe-modules/apis/network';
 
-export async function getListFromDynamoDB(table: string): Promise<AxiosResponse> {
-  return await aws.get(`/dynamoDB/${table}`);
+export async function getListFromDynamoDB(table: string, additionalKeys?: string): Promise<AxiosResponse> {
+  return await aws.get(`/dynamoDB/${table}?additionalKeys=${additionalKeys ?? ''}`);
 }
-export async function getFromDynamoDB(table: string, key: string): Promise<AxiosResponse> {
-  return await aws.get(`/dynamoDB/${table}/${key}`);
+export async function getFromDynamoDB(table: string, key: string, additionalKeys?: string): Promise<AxiosResponse> {
+  return await aws.get(`/dynamoDB/${table}/${key}?additionalKeys=${additionalKeys ?? ''}`);
 }
 export async function postFromDynamoDB(table: string, item: any): Promise<AxiosResponse> {
   return await aws.post(`/dynamoDB/${table}`, item);
