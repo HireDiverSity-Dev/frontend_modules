a5bafa555079c352b9f0e5462c5aafa3fc0ee6e3
Developerryou <developerryou@gmail.com>
2023-07-26 16:34:04 +0900

    FIX: 자동저장 기능 수정

--- components/FormPage/index.tsx
+++ components/FormPage/index.tsx
@@ -17,64 +17,52 @@ function getEndPage(forms: Array<FormUISetting>): number {
 
 function FormPage({ props, auth }: { props: FormPageProps; auth: Auth }) {
   const { i18n } = useTranslation(['customForm', 'form', 'common']);
+  let savedValues: any = {};
+  if (typeof window !== 'undefined') savedValues = JSON.parse(localStorage.getItem(props._id) ?? '{}');
 
   const form = useForm({
     mode: 'onBlur',
     reValidateMode: 'onChange',
+    defaultValues: savedValues,
   });
   const { getValues, reset } = form;
 
   const [page, setPage] = useState(1);
-  // Todo: any 타입 제거
-  const [defaultValues, setDefaultValues] = useState<any>({});
 
   useEffect(() => {
-    const AutoSaveForm = () => {
-      localStorage.setItem(props.path, JSON.stringify(getValues()));
-      console.log('임시 저장 완료');
-    };
-    const id = setInterval(AutoSaveForm, 5 * 60 * 1000);
-    return () => clearInterval(id);
-  }, []);
+    localStorage.setItem(props._id, JSON.stringify(getValues()));
+  }, [form.watch()]);
 
   // Todo: 함수 분리
   useEffect(() => {
-    // 프리필 항목 설정: 자동저장 불러오기 -> url query -> 기본 세팅값 -> undefined 순
-    let newDefaultValues: any = {};
+    // 프리필 항목 설정: url query 로 들어오는 값 추가 -> 기본 세팅값 추가 -> undefined 순
+    let newDefaultValues: any = { ...savedValues };
     newDefaultValues.saveDir = props.directory;
 
-    const saved = JSON.parse(localStorage.getItem('form') ?? '{}');
     const preFills = new URLSearchParams(location.search);
+    console.log(preFills);
+
     props.forms.forEach((formUI) => {
-      // url encode 여부 확인
-      const keys: string[] = [
+      const _id = formUI.FormItem_id;
+      const encodedKeys: string[] = [
         formUI.FormItem_id,
         encodeURIComponent(formUI.FormItem_id),
         formUI.FormItem_id.replace(/[(]/g, '%28').replace(/[)]/g, '%29'),
       ];
 
-      let defaultValue = saved[keys[0]]
-        ? saved[keys[0]] // 자동저장된 값
-        : preFills.get(keys[0]) && typeof preFills.get(keys[0]) !== null // url로 지정된 값
-        ? decodeURIComponent(preFills.get(keys[0]) as string)
-        : preFills.get(keys[1]) && typeof preFills.get(keys[1]) !== null
-        ? decodeURIComponent(preFills.get(keys[1]) as string)
-        : preFills.get(keys[2]) && typeof preFills.get(keys[2]) !== null
-        ? decodeURIComponent(preFills.get(keys[2]) as string)
-        : formUI.defaultValue; // 폼 설정 시 default로 설정한 값
-      newDefaultValues[keys[0]] = defaultValue;
+      if (newDefaultValues[_id] !== undefined) return; // 1순위 : 자동저장된 값
+      for (let key of encodedKeys) {
+        // 2순위 : url로 들어오는 값
+        if (preFills.get(key) && typeof preFills.get(key) !== null) {
+          newDefaultValues[key] = decodeURIComponent(preFills.get(key) as string);
+          return;
+        }
+      }
+      newDefaultValues[_id] = formUI.defaultValue; // 3순위 : 기본값
     });
 
-    setDefaultValues(newDefaultValues);
-    reset(defaultValues, {
-      keepValues: true,
-    });
+    reset(newDefaultValues);
   }, []);
-
-  // 프리필
-  useEffect(() => {
-    reset(defaultValues);
-  }, [defaultValues]);
 
   return (
     <>
