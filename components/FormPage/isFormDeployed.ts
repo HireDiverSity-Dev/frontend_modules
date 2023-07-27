9d06c136d3dc328e0999b6442e95b4372c8f94c3
Developerryou <developerryou@gmail.com>
2023-07-27 10:15:43 +0900

    FIX: 배포상태 확인 로직 수정

--- components/FormPage/isFormDeployed.ts
+++ components/FormPage/isFormDeployed.ts
@@ -1,15 +1,14 @@
 import { FormPageProps } from 'fe-modules/models/FormPage/FormPage';
 
 export default function isFormDeployed(props: FormPageProps) {
-  console.log(props.isDeployed, props.startDate, props.endDate);
   if (props.isDeployed === true) return true;
 
-  if (props.startDate === undefined) return false;
+  if (props.startDate === undefined || props.startDate === '') return false;
   const nowDate = new Date();
   const startDate = new Date(props.startDate);
   if (nowDate < startDate) return false;
 
-  if (props.endDate === undefined) return true;
+  if (props.endDate === undefined || props.endDate === '') return true;
   const endDate = new Date(props.endDate);
   if (nowDate > endDate) return false;
   return true;
