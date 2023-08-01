a8c03794a1b089809030763b02e632a0237b7862
Developerryou <developerryou@gmail.com>
2023-08-01 11:21:53 +0900

    FIX: url로 들어오는 값 파싱 수정

--- components/FormPage/getDefaultData.ts
+++ components/FormPage/getDefaultData.ts
@@ -1,4 +1,6 @@
+import { getFromDynamoDB } from 'fe-modules/apis/dynamoDB/table';
 import { FormPageProps } from 'fe-modules/models/FormPage/FormPage';
+import { FormUIData } from 'fe-modules/models/FormUI/FormUIData';
 
 export default function getDefaultData(props: FormPageProps, savedValues: any) {
   // 프리필 항목 설정: url query 로 들어오는 값 추가 -> 기본 세팅값 추가 -> undefined 순
@@ -7,21 +9,16 @@ export default function getDefaultData(props: FormPageProps, savedValues: any) {
 
   const preFills = new URLSearchParams(location.search);
 
-  props.forms.forEach((formUI) => {
+  props.forms.forEach(async (formUI) => {
     const _id = formUI.FormItem_id;
-    const encodedKeys: string[] = [
-      formUI.FormItem_id,
-      encodeURIComponent(formUI.FormItem_id),
-      formUI.FormItem_id.replace(/[(]/g, '%28').replace(/[)]/g, '%29'),
-    ];
 
     if (newDefaultValues[_id] !== undefined) return; // 1순위 : 자동저장된 값
-    for (let key of encodedKeys) {
-      // 2순위 : url로 들어오는 값
-      if (preFills.get(key) && typeof preFills.get(key) !== null) {
-        newDefaultValues[key] = decodeURIComponent(preFills.get(key) as string);
-        return;
-      }
+    // 2순위 : url로 들어오는 값
+    const formItem = (await getFromDynamoDB('FormItem', _id)).data as FormUIData;
+    const name = formItem.name;
+    if (preFills.get(name) && typeof preFills.get(name) !== null) {
+      newDefaultValues[_id] = decodeURIComponent(preFills.get(name) as string);
+      return;
     }
     newDefaultValues[_id] = formUI.defaultValue; // 3순위 : 기본값
   });
