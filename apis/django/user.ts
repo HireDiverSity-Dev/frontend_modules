04dd6ed497af1fe15d54b43aaf7f13c609bcae40
HireDiversity <official@hirediversity.club>
2023-08-14 12:37:54 +0900

    FIX: 서류제출완료 순번 API URL 오타 수정

--- apis/django/user.ts
+++ apis/django/user.ts
@@ -1,5 +1,5 @@
 import { django } from '../network';
 
 export default async function getOrder(email: string, order_type: string, autoAdd: boolean = false) {
-  return await django.get(`/user/${email}/order/${order_type}/autoAdd=${autoAdd}`);
+  return await django.get(`/user/${email}/order/${order_type}/?autoAdd=${autoAdd}`);
 }
