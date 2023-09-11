307d87d164709380a67616ca1549dde396be4bd9
HireDiversity <official@hirediversity.club>
2023-09-11 20:59:03 +0900

    HOT-FIX: 자체폼 예시 이미지 클릭 시 링크 이동하는 문제 수정

--- components/FormUI/ExampleImg.tsx
+++ components/FormUI/ExampleImg.tsx
@@ -1,12 +1,13 @@
 import { Button, Typography } from '@mui/material';
 import FlexBox from 'fe-modules/components/basic/FlexBox';
 import { useModal } from 'fe-modules/hooks/useModal';
+import { isMobile } from 'react-device-detect';
 
 const ExampleImg = ({ imgSrc }: { imgSrc: string }) => {
-  const { openModal } = useModal();
+  const { openModal, closeModal } = useModal();
 
   const onClick = () => {
-    openModal(<FullImage />, { maxWidth: '90vw', height: '90vh' });
+    openModal(<FullImage />);
   };
 
   const FullImage = () => {
@@ -16,21 +17,26 @@ const ExampleImg = ({ imgSrc }: { imgSrc: string }) => {
           position: 'relative',
           flexDirection: 'column',
           width: '100%',
-          height: '80vh',
           gap: 1,
           p: 3,
         }}
       >
-        <a href={imgSrc} target="_blank">
-          <Button variant="contained">원본 이미지 보기</Button>
-        </a>
+        <FlexBox sx={{ gap: 2, mb: 3 }}>
+          {!isMobile && (
+            <a href={imgSrc} target="_blank">
+              <Button variant="outlined">View original</Button>
+            </a>
+          )}
+          <Button variant="contained" onClick={closeModal}>
+            Close
+          </Button>
+        </FlexBox>
         <img
           src={imgSrc}
           style={{
             position: 'relative',
-            height: '100%',
+            maxWidth: '100%',
             maxHeight: '100%',
-            objectFit: 'contain',
           }}
         />
       </FlexBox>
@@ -39,26 +45,24 @@ const ExampleImg = ({ imgSrc }: { imgSrc: string }) => {
 
   return (
     <FlexBox sx={{ mb: '10px' }}>
-      <a href={imgSrc} target="_blank">
-        <div style={{ position: 'relative', width: 'auto', cursor: 'pointer' }}>
-          <img src={imgSrc} style={{ height: 200, objectFit: 'contain' }} onClick={onClick} />
-          <Typography
-            variant="caption"
-            sx={{
-              position: 'absolute',
-              top: '0%',
-              left: '100%',
-              transform: 'translate(-100%, 0%)',
-              backgroundColor: 'white',
-              border: 'dashed 1px',
-              px: '5px',
-              py: '3px',
-            }}
-          >
-            Click
-          </Typography>
-        </div>
-      </a>
+      <div style={{ position: 'relative', width: 'auto', cursor: 'pointer' }}>
+        <img src={imgSrc} style={{ height: 200, objectFit: 'contain' }} onClick={onClick} />
+        <Typography
+          variant="caption"
+          sx={{
+            position: 'absolute',
+            top: '0%',
+            left: '100%',
+            transform: 'translate(-100%, 0%)',
+            backgroundColor: 'white',
+            border: 'dashed 1px',
+            px: '5px',
+            py: '3px',
+          }}
+        >
+          Click
+        </Typography>
+      </div>
     </FlexBox>
   );
 };
